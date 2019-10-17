652bd7d9e591ba9d3893f9b82d3f7f39fa2c866d
Tony Mobily <tonymobily@gmail.com>
2019-10-17 08:40:02 +0800

    Material theme renamed

--- designers/loader.js
+++ designers/loader.js
@@ -7,7 +7,7 @@
 const script1 = window.document.createElement('SCRIPT')
 script1.setAttribute('src', 'https://unpkg.com/@webcomponents/webcomponentsjs/webcomponents-bundle.js')
 const script2 = window.document.createElement('SCRIPT')
-script2.setAttribute('src', 'https://unpkg.com/tpe/distr/material.js')
+script2.setAttribute('src', 'https://unpkg.com/tpe/distr/theme-material.js')
 const script3 = window.document.createElement('SCRIPT')
 script3.setAttribute('src', 'https://unpkg.com/tpe/distr/tpe.js')
 window.document.head.appendChild(script1)
